763ac18c939bb44dbb870ec26b11378673efa992
hanjiahui <xiaoyanrujiu666@163.com>
2022-07-21 10:26:40 +0800

    添加页面

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  base: "./", // 配置打包文件路径
+  server: {
+    open: true, // 在默认浏览器中自动打开该地址
+  },
+});
